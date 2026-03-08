5137115df2831c3f87f5733d060b8dbd36373579
alexzeey2 <151472189+alexzeey2@users.noreply.github.com>
2026-03-08 16:06:35 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        product: resolve(__dirname, 'product.html'),
+        seller: resolve(__dirname, 'seller.html'),
+      }
+    }
+  }
 })
